39f01206abcccc3238ae66d4b7986c4c49e37fbd
Mandeep Kaur <kaurm070795@gmail.com>
2019-02-10 10:35:00 -0500

    order detail

--- Team01activity/src/team01activity/Order_Detail.java
+++ Team01activity/src/team01activity/Order_Detail.java
@@ -9,15 +9,37 @@ package team01activity;
  *
  * @author Mandeep Kaur
  */
-public class Order_Detail {
+public class Order_Detail extends Order{
     int Quantity;
     String tax_Status;
     
-    public Order_Detail (int Quantity, String tax_Status)
+    
+    public void Order(int Quantity,String tax_Status)
     {
         this.Quantity=Quantity;
         this.tax_Status=tax_Status;
+       
     }
+    public void Settax_Status(String t)
+    {
+        this.tax_Status=t;
+    }
+    public String gettax_Status()
+    {
+        return tax_Status;
+    }
+    
+    
+    
+    public void setQuantity(int Q)
+    {
+        this.Quantity=Q;
+    }
+    public int getQuantity()
+    {
+        return Quantity;
+    }
+
     
     public void calc_subtotal()
     {
@@ -29,7 +51,11 @@ public class Order_Detail {
     }
        public void calc_total()
        {
+           }
+    public void display()
+    {
+        System.out.println("Order Quantity  is:"+Quantity  +"tax_Status"+tax_Status);
+    }
     
-}
 }
 
