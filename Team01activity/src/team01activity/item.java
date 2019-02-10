b693c73274605e896253e536cce4532b6139e8ba
Mandeep Kaur <kaurm070795@gmail.com>
2019-02-10 10:58:06 -0500

    order detail

---
+++ Team01activity/src/team01activity/item.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team01activity;
+
+/**
+ *
+ * @author Mandeep Kaur
+ */
+public class item extends Order_Detail {
+    
+    
+    
+    
+    
+    
+  
+    public void get_PriceforQuantity()
+    {
+                                                                                
+    }
+    public void get_tax()
+    {
+
+    }
+       public void in_stock()
+       {
+    
+}
+}
+
+    
+
