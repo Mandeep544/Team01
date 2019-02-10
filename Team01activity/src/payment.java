762d1aa0a65d0f6b62d094467a8856515d4326e3
Sharad <.gitsharadphw069@gmail.com>
2019-02-10 09:50:10 -0500

    Merge origin/master

---
+++ Team01activity/src/payment.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author temp
+ */
+public class payment {
+    
+    float amount;
+    
+    public payment(float theamount)
+    {
+        this.amount=theamount;
+    }
+    
+    public void setamount(float amount)
+    {
+        this.amount=amount;
+    }
+    public float getamount()
+    {
+        return amount;
+    }
+}
