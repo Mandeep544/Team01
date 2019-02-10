b4d5dc3e5999b3c1d298c81d5e3fda4d815645cc
1894343 <1894343@E210R12.CEGEPGIM.LCL>
2019-02-10 11:01:51 -0500

    initialising

---
+++ Team01activity/src/team01activity/cash1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team01activity;
+
+/**
+ *
+ * @author 1894343
+ */
+public class cash1 extends Payment {
+
+    public cash1(float theamount) {
+        super(theamount);
+    }
+    {
+        System.out.println("cash_tendered:");
+    }
+}
+
